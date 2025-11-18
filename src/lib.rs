dac43eb2c5a41ff00561ae936a43cab5ef4af56e
Allister Isaiah Harvey <41553768+martial-plains@users.noreply.github.com>
2025-11-18 13:57:41 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -55,7 +55,7 @@
 //! For example, the following code specifies the order for Latin, Numbers, and Punctuation character sets:
 //!
 //! ```rust
-//! use morsify::MorseCharacterSet;
+//! use morsify::{MorseCharacterSet, Options};
 //!
 //! let options = Options {
 //!     character_set_order: vec![
